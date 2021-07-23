97bab55c71f447659f53a7e931a67cdce52940d2
ricardovanlaarhoven <ricardo1994@gmail.com>
2021-07-23 11:33:10 +0200

    refactor(posts): create a seperate component

---
+++ src/components/PostsCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <VSkeletonLoader type="list-item-two-line@6" v-if="isLoading"/>
+      <v-list v-else>
+        <v-list-item v-for="post in posts" :key="post.id">
+          <v-list-item-content>
+            <v-list-item-title>{{ post.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ dayjs(post.date).format('LL') }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+    <AddPostDialog @add="addPost" v-model="dialog"/>
+    <v-btn fab absolute bottom right color="primary" @click="openPostDialog">
+      <v-icon>mdi-pencil-plus-outline</v-icon>
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+import dayjs from '@/plugins/dayjs.js';
+import { postCollection } from '@/collections/posts.js';
+import AddPostDialog from '@/components/addPostDialog.vue';
+
+export default {
+  name: 'PostsCard',
+  components: { AddPostDialog },
+  created() {
+    this.fetch();
+  },
+  data: () => ({
+    isLoading: true,
+    posts: [],
+    dialog: false,
+  }),
+  methods: {
+    dayjs,
+    openPostDialog() {
+      this.dialog = true;
+    },
+    addPost(post) {
+      this.posts.push(post);
+    },
+    async fetch() {
+      this.posts = [];
+      this.isLoading = true;
+      const response = await postCollection.get();
+      response.forEach((post) => {
+        const data = post.data();
+        this.posts.push({
+          id: post.id,
+          title: data.title,
+          date: data.date.toDate(),
+        });
+      });
+
+      this.isLoading = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,61 +1,16 @@
 <template>
   <v-row>
     <v-col cols="12" lg="4">
-      <v-card>
-        <v-card-text>
-          <v-list>
-            <v-list-item v-for="post in posts" :key="post.id">
-              <v-list-item-content>
-                <v-list-item-title>{{ post.title }}</v-list-item-title>
-                <v-list-item-subtitle>{{ dayjs(post.date).format('LL') }}</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-card-text>
-        <AddPostDialog @add="addPost" v-model="dialog"/>
-        <v-btn fab absolute bottom right color="primary" @click="openPostDialog">
-          <v-icon>mdi-pencil-plus-outline</v-icon>
-        </v-btn>
-      </v-card>
+      <PostsCard/>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import dayjs from '@/plugins/dayjs.js';
-import { postCollection } from '@/collections/posts.js';
-import AddPostDialog from '@/components/addPostDialog.vue';
 
+import PostsCard from '@/components/PostsCard.vue';
 export default {
   name: 'Home',
-  components: { AddPostDialog },
-  created() {
-    this.fetch();
-  },
-  data: () => ({
-    posts: [],
-    dialog: false,
-  }),
-  methods: {
-    dayjs,
-    openPostDialog() {
-      this.dialog = true;
-    },
-    addPost(post) {
-      this.posts.push(post);
-    },
-    async fetch() {
-      this.posts = [];
-      const response = await postCollection.get();
-      response.forEach((post) => {
-        const data = post.data();
-        this.posts.push({
-          id: post.id,
-          title: data.title,
-          date: data.date.toDate(),
-        });
-      });
-    },
-  },
+  components: { PostsCard },
 };
 </script>
